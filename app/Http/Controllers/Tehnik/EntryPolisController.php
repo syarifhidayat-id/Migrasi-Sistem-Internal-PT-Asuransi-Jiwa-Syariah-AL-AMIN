e607744c14e8ba954aadbf352ec10fbd61923e94
Doni Yanuar <doniyanuar18@gmail.com>
2022-11-04 16:59:49 +0700

    entry polis rev

--- app/Http/Controllers/Tehnik/EntryPolisController.php
+++ app/Http/Controllers/Tehnik/EntryPolisController.php
@@ -94,47 +94,128 @@ class EntryPolisController extends Controller
      */
     public function store(Request $request)
     {
-        // $validasi = Validator::make($request->all(), [
-        //     'email' => 'required',
-        //     'no_hp' => 'required',
-        //     'email_user' => 'required',
-        //     'email_cc' => 'required',
-        //     'name' => 'required',
-        //     'password' => 'required',
-        //     'menu_tipe' => 'required',
-        // ]);
+        $validasi = Validator::make($request->all(), [
+            'mpol_mrkn_nama' => 'required',
+        ]);
 
-        // if ($validasi->fails()) {
+        if ($validasi->fails()) {
+            return response()->json([
+                'error' => $validasi->errors()
+            ]);
+        }
+
+        if ($request->mpol_kode == "") {
+            $kode = KodeController::__getKey(14);
+            $data = $request->all();
+            $data = $request->except('_token');
+
+            $data['mpol_kode'] = $kode;
+            $data['mpol_mrkn_nama'] = $request->mpol_mrkn_nama;
+            // $request->merge([
+            //         'mpol_kode' => $kode,
+            //         'mpol_kode_ori'  => '0',
+            //         'mpol_endos' => '0',
+            //         'mpol_aktif' => '0',
+            //         'mpol_mekanisme' => '0',
+            //         'mpol_mekanisme2' => '0',
+            //         'mpol_endos_idx' => '0',
+            //         'mpol_file_page' => '0',
+            //         'mpol_max_up' => '0',
+            //         'mpol_openpolis' => '0',
+            //         'mpol_mgol_kode' => '0',
+            //         'mpol_mgp_kode' => '0',
+            //         'mpol_mgs_kode' => '0',
+            //         'mpol_mlu_kode' => '0',
+            //         'mpol_jns_perusahaan' => '0',
+            //         'mpol_mja_kode' => '0',
+            //         'mpol_mgpp_kode' => '0',
+            //         'mpol_usia_min' => '0',
+            //         'mpol_usia_max' => '0',
+            //         'mpol_jatuh_tempo' => '0',
+            //         'mpol_lapor_data' => '0',
+            //         'mpol_lapor_stnc' => '0',
+            //         'mpol_byr_premi' => '0',
+            //         'mpol_mujh_persen' => '0',
+            //         'mpol_mkomdisc_persen' => '0',
+            //         'mpol_referal' => '0',
+            //         'mpol_maintenance' => '0',
+            //         'mpol_pajakfee' => '0',
+            //         'mpol_handlingfee' => '0',
+            //         'mpol_pajakfee_persen' => '0',
+            //         'mpol_pajakkom_persen' => '0',
+            //         'mpol_jns_tarif' => '0',
+            //         'mpol_max_pst' => '0',
+            //         'mpol_tipe_uw'=> '0',
+            //         'mpol_kadaluarsa_klaim'=> '0',
+            //         'mpol_mmft_kode_gu'=> '0',
+            //         'mpol_mmft_kode_wp_swasta'=> '0',
+            //         'mpol_mmft_kode_wp_pns'=> '0',
+            //         'mpol_mmft_kode_phk_pensiun'=> '0',
+            //         'mpol_mmft_kode_phk_swasta'=> '0',
+            //         'mpol_mmft_kode_phk_pns'=> '0',
+            //         'mpol_mmft_kode_tlo'=> '0',
+            //         'mpol_mmft_kode_fire'=> '0',
+            //         'mpol_mmft_kode_jiwa'=> '0',
+            //         'mpol_approve'=> '0',
+            //         'mpol_ins_date'=> date('Y-m-d'),
+            //         'mpol_ins_user'=> '0',
+            //         'mpol_upd_date'=> date('Y-m-d'),
+            //         'mpol_upd_user'=> '0',
+            //         'mpol_jl'=> '0',
+            //         'mpol_jl_pst'=> '0',
+            //         'mpol_jl_pasangan'=> '0',
+            //         'mpol_aprove_fc'=> '0',
+            //         'mpol_sts_akun'=> '0',
+            //         'mpol_jenis_cetak'=> '0',
+            //         'mpol_jenis_bayar'=> '0',
+            //         'mpol_cetak_lunas'=> '0',
+            //         'mpol_online'=> '0',
+            //         'mpol_online_individual'=> '0',
+            //         'mpol_online_rekan'=> '0',
+            //         'mpol_surat_tambah'=> '0',
+            //         'mpol_va'=> '0',
+            //         'mpol_ketagihan'=> '0',
+            //         'mpol_indexfolder'=> '0',
+            //         'mpol_surplus'=> '0',
+            //         'mpol_disc_lain'=> '0',
+            //         'mpol_nomor_cetak'=> '0',
+            //         'mpol_jenis_login'=> '0',
+            //         'mpol_alias'=> '0',
+            //         'mpol_status_polis'=> '0',
+            //         'mpol_note'=> '0',
+            //         'mpol_fee_tampil'=> '0',
+            //         'mpol_ujroh_treaty'=> '0',
+            //         'mpol_trbr_date'=> date('Y-m-d'),
+            //         'mpol_sync'=> '0',
+            //         'mpol_payonline'=> '0',
+            //         'mpol_agent'=> '0',
+            //         'mpol_pemgroupusaha'=> '0',
+            //         'mpol_standar_perlindungan'=> '0',
+            //         'mpol_standar_premi'=> '0',
+            //         'mpol_host2host'=> '0',
+            //         'mpol_convert_img'=> '0',
+            //         'mpol_iscopy'=> '0',
+            //         'mpol_pilihan_cetak'=> '0',
+            //         'mpol_mjasu_kode'=> '0',
+            //         'mpol_acc_tek'=> '0',
+            // ])->except('_token');
+
+            // return $request;
+            // Polis::create($request->all());
+            $vTable = DB::table('eopr.mst_polis');
+            $vTable->insert($data);
+            return response()->json([
+                'success' => 'Data berhasil disimpan dengan Kode '.$kode.'!'
+            ]);
+            // return $data;
+
+        }
+        // else {
+        //     $menu = Polis::findOrFail($request->wmn_kode);
+        //     $menu->update($request->all());
+
         //     return response()->json([
-        //         'error' => $validasi->errors()
-        //     ]);
-        // }
-
-        // if ($request->id == "") {
-        //     $kode = KodeController::__getPK(User::all()->max('id'), 15);
-
-        //     // if ($request->wmn_url == "") {
-        //     //     $request->merge([
-        //     //         'wmn_url' => 'maintenance',
-        //     //     ]);
-        //     // }
-
-        //     $request->merge([
-        //         'id' => $kode,
-        //     ]);
-
-        //     User::create($request->all());
-
-        //     return response()->json([
-        //         'success' => 'Data berhasil disimpan dengan Kode '.$kode.'!'
-        //     ]);
-
-        // } else {
-        //     $user = User::findOrFail($request->id);
-        //     $user->update($request->all());
-
-        //     return response()->json([
-        //         'success' => 'Data berhasil diupdate dengan Kode '.$request->id.'!'
+        //         'success' => 'Data berhasil diupdate dengan Kode '.$request->wmn_kode.'!'
         //     ]);
         // }
     }
--- routes/web/tehnik.php
+++ routes/web/tehnik.php
@@ -9,13 +9,34 @@ use App\Http\Controllers\Utility\WewenangJabatanController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/tehnik', 'as' => 'tehnik.'], function () {
-
-  Route::group(['prefix' => '/polis', 'as' => 'polis.'], function () {
     Route::resource('/lihat-polis', PolisController::class);
     Route::resource('/entry-master-polis', EntryPolisController::class);
-  });
+    // Route::get('/entry-master-polis', [EntryPolisController::class, 'index'])->name('entry-master-polis.index');
+    // Route::post('/entry-master-polis/store', [EntryPolisController::class, 'store'])->name('entry-master-polis.store');
 
-    
-    
+//   Route::group(['prefix' => '/polis', 'as' => 'polis.'], function () {
+//     Route::resource('/lihat-polis', PolisController::class);
+//     Route::resource('/entry-master-polis', EntryPolisController::class);
+//     // Route::prefix('/entry-master-polis')->name('entry-master-polis.')->controller(EntryPolisController::class)->group(function() {
+//     //     Route::post('/store', 'store');
+//     // });
+//   });
+
+
+
 });
+
+
+// Route::group(['prefix' => '/tehnik', 'as' => 'tehnik.'], function () {
+
+//     Route::resource('/lihat-polis', PolisController::class);
+//     Route::resource('/entry-master-polis', EntryPolisController::class);
+//     // Route::prefix('/menu')->name('menu.')->controller(MenuController::class)->group(function() {
+//     //     Route::get('/getTipe/{id}', 'getTipemenu');
+//     //     Route::get('/edit/{id}', 'edit');
+//     //     Route::get('/keyMenu/{id}', 'keyMenu');
+//     // });
+//     // Route::resource('users', UserController::class);
+//     // Route::resource('permissions', PermissionController::class);
+// });
 
